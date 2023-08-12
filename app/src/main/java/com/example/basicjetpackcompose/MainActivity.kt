415ccf75f659d1703eed2e376ebd8690291ff12f
Abinash Acharya <075bei004.abinash@pcampus.edu.np>
2023-08-12 17:58:48 +0545

    Basic Learnings.

--- app/src/main/java/com/example/basicjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/basicjetpackcompose/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,16 +43,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        color = Color.Blue,
-        fontSize = 30.sp,
-        //The modifier code is ran sequentially.
+    /*Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = Modifier
-            .background(Color.Red)
-            .padding(16.dp)
-            .background(Color.Green)
+            //.fillMaxHeight()
+            //.fillMaxWidth()
+            .size(400.dp)
+            //.fillMaxSize()
+            //.background(Color.White)
+
+    )*/
+    Box(
+        modifier = Modifier
+            .size(400.dp)
     )
+    {
+        Text(
+            text = "Hello $name!",
+            color = Color.Blue,
+            fontSize = 30.sp,
+            //The modifier code is ran sequentially.
+//            modifier = Modifier
+//                .background(Color.Red)
+//                .padding(16.dp)
+//                .background(Color.Green)
+        )
+        Text(
+            text = "Some other text!!",
+            color = Color.Blue,
+            fontSize = 30.sp,
+            //The modifier code is ran sequentially.
+//            modifier = Modifier
+//                .background(Color.Red)
+//                .padding(16.dp)
+//                .background(Color.Green)
+        )
+    }
 }
 //Layout Initial Inspector
 @Preview(showBackground = true)
