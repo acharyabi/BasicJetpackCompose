73520d573171d703ab748ddfae28a235f98cfbaf
Abinash Acharya <075bei004.abinash@pcampus.edu.np>
2023-08-15 23:13:06 +0545

    Adding simple buttons to simply add the text in compose.

--- app/src/main/java/com/example/basicjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/basicjetpackcompose/MainActivity.kt
@@ -8,17 +8,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,12 +43,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.basicjetpackcompose.ui.theme.BasicJetpackComposeTheme
+import org.w3c.dom.NameList
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BasicJetpackComposeTheme {
+                /*
                 // A surface container using the 'background' color from the theme
                 //Surface(
                 var count by remember {
@@ -60,10 +70,62 @@ class MainActivity : ComponentActivity() {
                     Button(onClick ={
                         count += 1
                     }){
-                        Text(text = "Click Me!")
+                        Text(text = "Click Me: $count")
+                    }*/
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+                Column (
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ){
+                    Row (
+                        modifier =Modifier.fillMaxWidth()
+                    ){
+                        OutlinedTextField(
+                            value =name,
+                            onValueChange ={ text ->
+                                name = text
+                            },
+                            modifier = Modifier
+                                .weight(1f),
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            if(name.isNotBlank()) {
+                                names = names + name
+                                name =""    //Clear the input field after inputting
+                            }
+                         }) {
+                            Text(text = "Add")
+                        }
                     }
+                    NameList(names = names)
+                }
                 }
             }
+        }
+    }
+
+@Composable
+fun NameList(
+    names: List<String>,
+    modifier: Modifier = Modifier
+){
+    LazyColumn(modifier){
+        items(names){
+                currentName ->
+            Text(
+                text = currentName,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            Divider()
         }
     }
 }
